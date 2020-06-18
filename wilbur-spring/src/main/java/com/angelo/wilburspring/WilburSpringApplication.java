0db4c3855189d103e556b05afcb86feb724673e5
Ryan Angelo <rangelo89@gmail.com>
2020-06-17 22:07:00 -0400

    Initial commit of wilbur. Spring and React projects added.

---
+++ wilbur-spring/src/main/java/com/angelo/wilburspring/WilburSpringApplication.java
@@ -0,0 +1,13 @@
+package com.angelo.wilburspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilburSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilburSpringApplication.class, args);
+	}
+
+}
